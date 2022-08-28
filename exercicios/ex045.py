aba5b368478df1fb211d56b2bb973810962ba0cd
Júlia Martins Orsi <102994938+Orsi88@users.noreply.github.com>
2022-08-28 00:41:40 -0300

    atualizei

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -21,42 +21,100 @@
 
 #iniciando jogo
 from time import sleep
-from random import randint
+import random
 import os
+
+pontoHumano = 0
+pontoMaquina = 0
+cont = 1
+
+while cont <= 3:
+
+    os.system('cls')
+
+    print('*'*20)
+    print('       JOKENPÔ       ')
+    print('*'*20)
+    print('\n  Você  ⚔️   Máquina  \n')
+
+    print('Rodada {}/3'.format(cont))
+
+    print('[a] 👊  - pedra')
+    print('[b] 🖐  - papel')
+    print('[c] ✌  - tesoura')
+
+    a = '👊'
+    b = '🖐'
+    c = '✌'
+
+    jogadas = [a, b, c]
+    jogadaHumano = str(input('Sua vez: '))
+
+    if jogadaHumano == 'a':
+        jogadaHumano = '👊'
+    elif jogadaHumano == 'b':
+        jogadaHumano = '🖐'
+    elif jogadaHumano == 'c':
+        jogadaHumano = '✌'
+
+    jogadaMaquina = random.choice(jogadas)
+
+    for c in range(3, 0, -1):
+        os.system('cls')
+        print('⏳   ', c, '   ⏳')
+        sleep(1)
+        os.system('cls')
+    print('JO  -  KEN  -  PÔ \n')
+    print(jogadaHumano, '     ⚔️     ', jogadaMaquina)
+
+    if jogadaHumano == '👊' and jogadaMaquina == '👊':
+        print('\nRodada empatada')
+        cont = cont + 0
+    elif jogadaHumano == '🖐' and jogadaMaquina == '🖐':
+        print('\nRodada empatada')
+        cont = cont + 0
+    elif jogadaHumano == '✌' and jogadaMaquina == '✌':
+        print('\nRodada empatada')
+        cont = cont + 0
+    elif jogadaHumano == '🖐' and jogadaMaquina == '👊':
+        pontoHumano = pontoHumano + 1
+        print('\nVocê venceu a rodada {}/3 👏'.format(cont))
+        cont = cont +1
+    elif jogadaHumano == '✌' and jogadaMaquina == '🖐':
+        pontoHumano = pontoHumano + 1
+        print('\nVocê venceu a rodada {}/3 👏'.format(cont))
+        cont = cont +1
+    elif jogadaHumano == '👊' and jogadaMaquina == '✌':
+        pontoHumano = pontoHumano + 1
+        print('\nVocê venceu a rodada {}/3 👏'.format(cont))
+        cont = cont +1
+    elif jogadaHumano == '👊' and jogadaMaquina == '🖐':
+        pontoMaquina = pontoMaquina + 1
+        print('\nA máquina venceu a rodada {}/3😘'.format(cont))
+        cont = cont +1
+    elif jogadaHumano == '🖐' and jogadaMaquina == '✌':
+        pontoMaquina = pontoMaquina + 1
+        print('\nA máquina venceu a rodada {}/3😘'.format(cont))
+        cont = cont +1
+    elif jogadaHumano == '✌' and jogadaMaquina == '👊':
+        pontoMaquina = pontoMaquina + 1
+        print('\nA máquina venceu a rodada {}/3😘'.format(cont))
+        cont = cont +1
+
+    #a > b  and c > b and a > c  
+    sleep(3)
+
 
 print('*'*20)
 print('       JOKENPÔ       ')
 print('*'*20)
-print('\n  Você  ⚔️   Máquina  \n')
 
-print('Rodada 1/3')
-
-print('[1] 👊 - pedra')
-print('[2] 🖐 - papel')
-print('[3] ✌ - tesoura')
-
-jogadaHumano = str(input('Sua vez: '))
-jogadaMaquina = randint(1, 3)
-
-for c in range(3, 0, -1):
-    os.system('cls')
-    print('_'*10,c,'_'*10)
-    sleep(1)
-    os.system('cls')
-
-if jogadaHumano == '1':
-    print('Humano:👊')
-elif jogadaHumano == '2':
-    print('Humano:🖐')
-elif jogadaHumano == '3':
-    print('Humano:✌')
-
-if jogadaMaquina == '1':
-    print('Máquina:👊')
-elif jogadaMaquina == '2':
-    print('Máquina:🖐')
-elif jogadaMaquina == '3':
-    print('Máquina:✌')
+if pontoHumano > pontoMaquina:
+    print('Parabéns, você veceu a Partida!!')
+    print('🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆')
+else:
+    print('A máquina venceu a Partida!!')
+    print('💔💔💔💔💔💔💔💔')
 
 
 
