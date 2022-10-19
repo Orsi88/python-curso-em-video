6885554b60d3eb88407e3d6c38fba774da8d6ed7
Júlia Martins Orsi <102994938+Orsi88@users.noreply.github.com>
2022-10-19 00:40:19 -0300

    atualizando curso

--- exercicios/ex059.py
+++ exercicios/ex059.py
@@ -1,52 +1,36 @@
-# Calculadora simples
+# Calculadora Simples
+
+# Função para receber os valores
+
+valor1 = float(input('Digite um número: '))
+valor2 = float(input('Digite outro número: '))
+
+# Manipulando valores
+valores = []    
+valores.insert(0, valor1)
+valores.insert(0, valor2)
+
+print(valores)
+
+# Criação Menu
 
 escolha = ''
-valor1 = 0
-valor2 = 0
-resultado = 0
+resposta = 0
 
-# Recebendo info
-def recebendoInfo():
-    valor1 = float(input('Digite um número: '))
-    valor2 = float(input('Digite outro número: '))
-    
-    return valor1 and valor2
-# Criando Menu
+# Criação opções Menu
 
-def manipulandoQuestão():
-    print('-=-'*15)
-    print('[1] Somar')
-    print('[2] Mutiplicar')
-    print('[3] Maior')
-    print('[4] Novos números')
-    print('[5] Sair do programa')
-    print('-=-'*15)
+if escolha == '1':
+    resposta = valor1 + valor2
+elif escolha == '2':
+    resposta = valor1 * valor2
+elif escolha == '3':
+    if valor1 > valor2:
+        resposta = valor1
+    elif valor1 < valor2:
+        resposta = valor2
 
-    escolha = input('Você deseja realizar qual operação? ')
-    return escolha
 
-# Testando
+# Respetição Menu
+
 while escolha != '5':
-    recebendoInfo()
-    manipulandoQuestão()
-    if escolha == '1':
-        resultado = valor1 + valor2
-    elif escolha == '2':
-        resultado = valor1 * valor2
-        print(resultado)
-    elif escolha == '3':
-        if valor1 > valor2:
-            resultado = valor1
-            print(resultado)
-        elif valor2 > valor1:
-            resultado = valor2
-            print(resultado)
-        elif valor1 == valor2:
-            resultado = 'Os vaores são iguais'
-            print(resultado)
-    elif escolha == '4':
-        recebendoInfo()
-        manipulandoQuestão()
-    elif escolha == '5':
-        break
     
